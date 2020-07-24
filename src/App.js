7c42f02f068060976ee2ffb0a22f1d147047f592
wardatunnisa <wardatunnisa2001@gmail.com>
2020-07-24 20:08:21 +0700

    update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,23 @@ import { MenuItem, FormControl, Select, Card, CardContent } from "@material-ui/c
 import InfoBox from './InfoBox';
 import Map from './Map';
 import './App.css';
+import Table from "./Table";
+import { sortData } from "./util";
 
 function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState('worldwide');
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://disease.sh/v3/covid-19/all")
+      .then(response => response.json())
+      .then(data => {
+        setCountryInfo(data);
+      });
+  }, [])
+
   useEffect(() => {
     const getCountriesData = async () => {
       await fetch("https://disease.sh/v3/covid-19/countries")
@@ -19,6 +31,8 @@ function App() {
               value: country.countryInfo.iso2 // UK, USA, FR
             }));
 
+          const sortedData = sortData(data);
+          setTableData(sortedData);
           setCountries(countries);
         });
     };
@@ -38,7 +52,7 @@ function App() {
     await fetch(url)
       .then(response => response.json())
       .then(data => {
-        setCountry(CountryCode);
+        setCountry(countryCode);
 
         // All of the data
         // from the country response
@@ -77,8 +91,9 @@ function App() {
       <Card className="app_right">
         <CardContent>
           <h3>Live Cases by Country</h3>
-          {/* Table */}
+          <Table countries={tableData} />
           <h3>Worldwide new Cases</h3>
+          <LineGraph />
           {/* Graph */}
         </CardContent>
 
