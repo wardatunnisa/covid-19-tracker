692ac232f7e23d8d1fac06d129567a60a463bb4c
wardatunnisa <wardatunnisa2001@gmail.com>
2020-07-27 21:03:43 +0700

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Map from './Map';
 import './App.css';
 import Table from "./Table";
 import { sortData } from "./util";
+import LineGraph from "./LineGraph";
 
 function App() {
   const [countries, setCountries] = useState([]);
--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Line } from "react-chartjs-2";
 
 function LineGraph() {
+    const [data, setData] = useState({})
+
+    useEffect(() => {
+        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+            .then(response => response.json())
+            .then(data => {
+
+                console.log(data);
+
+            });
+    }, []);
+
+    const buildChartData = (data, casesType = 'cases') => {
+        const chartData = [];
+        let lastDataPoint;
+
+        data[casesType].forEach(date => {
+            if (lastDataPoint) {
+                const newDataPoint = {
+                    x: date,
+                    y: data['cases'][date] - lastDataPoint
+                };
+                chartData.push(newDataPoint);
+            }
+            lastDataPoint = data[casesType][date];
+        })
+        return chartData;
+    }
+
     return (
         <div>
-
+            <h1> I am a Graph</h1>
+            {/*<Line
+                data
+                options
+            />*/}
         </div>
     )
 }
